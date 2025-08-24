be20b4753105de1430ac8ed5eda6edb90958f393
ysoqrious <ysoqrious@gmail.com>
2025-08-24 17:09:22 +0900

    Modify : new print line added

--- Upload_test.py
+++ Upload_test.py
@@ -1,1 +1,4 @@
-print("A tset file for Github upload")
+print("A tset file for Github upload") # check for another commit
+
+for i in range(1, 3):
+    print(f'A new line for github commit {i} has a green vertical line')
